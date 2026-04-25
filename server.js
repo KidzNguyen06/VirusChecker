b5593d7c8ddb7dca26b2abe643f7334874bf9ee3
KidzNguyen06 <haona1606@gmail.com>
2026-04-25 11:49:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 const API_KEY = process.env.VT_API_KEY;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("OK chay thanh cong");
 });
